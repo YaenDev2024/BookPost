dfffd1fb0bfc926c9702d289cd45a1d184592989
tomacost <tomas.al.acosta@foxconn.com>
2024-08-06 05:46:38 -0600

    added new function likes, and comments count

--- BookPost/src/components/CardWithPubs.js
+++ BookPost/src/components/CardWithPubs.js
@@ -1,104 +1,231 @@
-import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import MaterialC from 'react-native-vector-icons/MaterialCommunityIcons';
-import { collection, query, where, onSnapshot } from '@firebase/firestore';
-import { db } from '../../config';
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  getDocs,
+  setDoc,
+  doc,
+  deleteDoc,
+} from '@firebase/firestore';
+import {db} from '../../config';
 import CommentsComp from './comments/CommentsComp';
+import {useAuth} from '../hooks/Autentication';
 
-const CardWithPubs = ({ img, data, name, id_pub }) => {
-    const [comments, setComments] = useState([]);
-    const [show, setShow] = useState(false);
+const CardWithPubs = ({img, data, name, id_pub}) => {
+  const [comments, setComments] = useState([]);
+  const [show, setShow] = useState(false);
+  const [isLiked, setLiked] = useState(false);
+  const {user} = useAuth();
+  const [userid, setUserID] = useState('');
 
-    useEffect(() => {
-        const commentsQuery = query(
-            collection(db, 'comments'),
-            where('id_pub', '==', id_pub)
-        );
-
-        const unsubscribe = onSnapshot(commentsQuery, (snapshot) => {
-            const commentsData = snapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-            setComments(commentsData);
-        }, (error) => {
-            console.error('Error al obtener los comentarios:', error);
-        });
-
-        // Cleanup function to unsubscribe from the listener when the component unmounts
-        return () => unsubscribe();
-    }, [id_pub]);
-
-    const handleShowComments = () => {
-        setShow(!show);
-    };
-
-    return (
-        <View style={styles.card}>
-            <View style={styles.headercard}>
-                <Image style={styles.imgPerfil} source={{ uri: img }} />
-                <Text style={styles.nameText}>{name}</Text>
-                <TouchableOpacity><MaterialC name="dots-vertical" size={25} color={'white'} /></TouchableOpacity>
-            </View>
-            <View style={styles.data}>
-                <Text style={{ color: 'white', fontWeight: '500' }}>{data}</Text>
-                <TouchableOpacity style={styles.dataCom} onPress={handleShowComments}>
-                    <Text style={styles.commnets}>{comments.length} comentario</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={styles.footercard}>
-                <TouchableOpacity style={{ marginRight: 10 }}><MaterialC name="cards-heart-outline" size={25} color={'#e3e3e3'} /></TouchableOpacity>
-                <TouchableOpacity style={{ marginRight: 10 }}><MaterialC name="message-outline" size={25} color={'#e3e3e3'} /></TouchableOpacity>
-                <TouchableOpacity style={{ marginRight: 10 }}><MaterialC name="share-variant-outline" size={25} color={'#e3e3e3'} /></TouchableOpacity>
-            </View>
-            <CommentsComp visible={show} onClose={handleShowComments} datacomments={comments} idpub={id_pub}/>
-        </View>
+  useEffect(() => {
+    const commentsQuery = query(
+      collection(db, 'comments'),
+      where('id_pub', '==', id_pub),
     );
+
+    const unsubscribe = onSnapshot(
+      commentsQuery,
+      snapshot => {
+        const commentsData = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setComments(commentsData);
+      },
+      error => {
+        console.error('Error al obtener los comentarios:', error);
+      },
+    );
+
+    return () => unsubscribe();
+  }, [id_pub]);
+
+  useEffect(() => {
+    getid();
+  }, []);
+
+  const getid = async () => {
+    const userSearch = query(
+      collection(db, 'users'),
+      where('mail', '==', user.email),
+    );
+    const userSnapshot = await getDocs(userSearch);
+    const userData = userSnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setUserID(userData[0]?.id);
+  };
+
+  useEffect(() => {
+    const likeQuery = query(
+      collection(db, 'likes_by_users'),
+      where('id_publication', '==', id_pub),
+      where('id_user', '==', userid),
+    );
+
+    const unsubscribe = onSnapshot(
+      likeQuery,
+      snapshot => {
+        const likesdata = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setLiked(likesdata.length > 0);
+      },
+      error => {
+        console.error('Error al obtener los likes:', error);
+      },
+    );
+
+    return () => unsubscribe();
+  }, [id_pub, userid]);
+
+  const handleShowComments = () => {
+    setShow(!show);
+  };
+
+  const saveLike = async () => {
+    const userSearch = query(
+      collection(db, 'users'),
+      where('mail', '==', user.email),
+    );
+    const userSnapshot = await getDocs(userSearch);
+    const userData = userSnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    if (userData.length > 0) {
+      const iduser = userData[0].id;
+
+      const likeQuery = query(
+        collection(db, 'likes_by_users'),
+        where('id_publication', '==', id_pub),
+        where('id_user', '==', iduser),
+      );
+
+      const likeSnapshot = await getDocs(likeQuery);
+
+      if (likeSnapshot.empty) {
+        const likedata = {
+          id_publication: id_pub,
+          id_user: iduser,
+        };
+
+        const likeDocRef = doc(collection(db, 'likes_by_users'));
+
+        try {
+          await setDoc(likeDocRef, likedata);
+          setLiked(true);
+        } catch (error) {
+          console.error('Error al guardar el like:', error);
+        }
+      } else {
+       
+        const likeDocId = likeSnapshot.docs[0].id;
+        try {
+          await deleteDoc(doc(db, 'likes_by_users', likeDocId));
+          setLiked(false);
+        } catch (error) {
+          console.error('Error al eliminar el like:', error);
+        }
+      }
+    } else {
+      console.error('No se encontraron datos del usuario.');
+    }
+  };
+
+  return (
+    <View style={styles.card}>
+      <View style={styles.headercard}>
+        <Image style={styles.imgPerfil} source={{uri: img}} />
+        <Text style={styles.nameText}>{name}</Text>
+        <TouchableOpacity>
+          <MaterialC name="dots-vertical" size={25} color={'white'} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.data}>
+        <Text style={{color: 'white', fontWeight: '500'}}>{data}</Text>
+        <TouchableOpacity style={styles.dataCom} onPress={handleShowComments}>
+          <Text style={styles.commnets}>{comments.length} comentario</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.footercard}>
+        {!isLiked ? (
+          <TouchableOpacity style={{marginRight: 10}} onPress={saveLike}>
+            <MaterialC name="cards-heart-outline" size={25} color={'#e3e3e3'} />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity style={{marginRight: 10}} onPress={saveLike}>
+            <MaterialC name="cards-heart" size={25} color={'red'} />
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity style={{marginRight: 10}}>
+          <MaterialC name="message-outline" size={25} color={'#e3e3e3'} />
+        </TouchableOpacity>
+        <TouchableOpacity style={{marginRight: 10}}>
+          <MaterialC name="share-variant-outline" size={25} color={'#e3e3e3'} />
+        </TouchableOpacity>
+      </View>
+      <CommentsComp
+        visible={show}
+        onClose={handleShowComments}
+        datacomments={comments}
+        idpub={id_pub}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: '#353535',
-        height: 'auto',
-        margin: -5,
-        width: '100%',
-        padding: 10,
-    },
-    headercard: {
-        flexDirection: 'row',
-        marginVertical: 0,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    imgPerfil: {
-        height: 40,
-        width: 40,
-        borderRadius: 20,
-        borderColor: 'white',
-        borderWidth: 1,
-    },
-    nameText: {
-        flex: 1,
-        marginLeft: 10,
-        color: 'white',
-        fontWeight: '500',
-    },
-    footercard: {
-        flexDirection: 'row',
-        padding: 10,
-        marginTop: -20,
-        width: '100%',
-    },
-    dataCom: {
-        padding: 10,
-        alignItems: 'flex-end',
-    },
-    data: {
-        padding: 10,
-    },
-    commnets:{
-        color:'white'
-    }
+  card: {
+    backgroundColor: '#353535',
+    height: 'auto',
+    margin: -5,
+    width: '100%',
+    padding: 10,
+  },
+  headercard: {
+    flexDirection: 'row',
+    marginVertical: 0,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  imgPerfil: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  nameText: {
+    flex: 1,
+    marginLeft: 10,
+    color: 'white',
+    fontWeight: '500',
+  },
+  footercard: {
+    flexDirection: 'row',
+    padding: 10,
+    marginTop: -20,
+    width: '100%',
+  },
+  dataCom: {
+    padding: 10,
+    alignItems: 'flex-end',
+  },
+  data: {
+    padding: 10,
+  },
+  commnets: {
+    color: 'white',
+  },
 });
 
 export default CardWithPubs;
--- BookPost/src/components/comments/CommentsComp.js
+++ BookPost/src/components/comments/CommentsComp.js
@@ -1,137 +1,232 @@
-import React, { useState } from 'react';
-import { Image, Modal, ScrollView, StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Image,
+  Modal,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import CommentUser from './CommentUser';
 import MaterialC from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useAuth } from '../../hooks/Autentication';
-import { addDoc, collection, getDocs, query, where, serverTimestamp } from '@firebase/firestore';
-import { db } from '../../../config';
+import {useAuth} from '../../hooks/Autentication';
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  serverTimestamp,
+  onSnapshot,
+  doc,
+  getDoc,
+} from '@firebase/firestore';
+import {db} from '../../../config';
 
-const CommentsComp = ({ visible, onClose, datacomments, idpub }) => {
-  
-    const { user } = useAuth();
-    const [comment, setComment] = useState('');
+const CommentsComp = ({visible, onClose, datacomments, idpub}) => {
+  const {user} = useAuth();
+  const [comment, setComment] = useState('');
+  const [morethanone, setMorethan] = useState(true);
+  const [isOneLike, setOneLike] = useState(false);
+  const [oneLike, setOnelikeName] = useState('');
+  const [allNames, setAllNames] = useState([]);
 
-    const saveComment = async () => {
-        try {
-            const commentsQuery = query(
-                collection(db, 'users'),
-                where('mail', '==', user.email)
-            );
-            const commentsSnapshot = await getDocs(commentsQuery);
-            const commentsData = commentsSnapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-
-            if (commentsData.length > 0) {
-                const { id, img_profile, username } = commentsData[0];
-
-                await addDoc(collection(db, 'comments'), {
-                    data: comment,
-                    date: serverTimestamp(),
-                    id_pub: idpub,
-                    id_user: id,
-                    img_perfil: img_profile,
-                    username: username,
-                });
-
-                // Opcional: limpiar el campo de comentario después de guardarlo
-                setComment('');
-            } else {
-                console.error('No user data found.');
-            }
-        } catch (error) {
-            console.error('Error al obtener los comentarios:', error);
-        }
-    }
-
-    const getLikesCount = () =>{
-        
-    }
-    return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={visible}
-            onRequestClose={onClose}
-        >
-            <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                    <TouchableOpacity onPress={onClose} style={styles.closeButton}>
-                        <MaterialC name="close" size={25} color={'white'} />
-                    </TouchableOpacity>
-                    {/* This is header of the comments section */}
-                    <View style={styles.headerlikes}>
-                        <MaterialC name="cards-heart" size={25} color={'red'} />
-                        <Text style={styles.headerText}>Tomas Acosta y 1234 personas...</Text>
-                    </View>
-                    <ScrollView style={{width:'100%'}}>
-                        {/* Comment by user */}
-                        {datacomments.map((item, index) => (
-                            <CommentUser key={index} data={item.data} user={item.username} img={item.img_perfil} date={item.date} />
-                        ))}
-                    </ScrollView>
-                    <View style={styles.inputs}>
-                        <TextInput style={styles.input} placeholder="Escribe un comentario..." placeholderTextColor={'gray'} value={comment} onChangeText={text => setComment(text)} />
-                        <TouchableOpacity onPress={() => saveComment()}>
-                            <MaterialC style={{ padding: 10, marginTop: 10 }} name="send" size={35} color={'white'} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        </Modal>
+  useEffect(() => {
+    const likeQuery = query(
+      collection(db, 'likes_by_users'),
+      where('id_publication', '==', idpub),
     );
-}
+
+    const unsubscribe = onSnapshot(
+      likeQuery,
+      async snapshot => {
+        const likesdata = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        if (likesdata.length === 0) {
+          setOneLike(false);
+          setAllNames([]);
+          setMorethan(false);
+        } else {
+          const namesPromises = likesdata.map(async element => {
+            const userDocRef = doc(db, 'users', element.id_user);
+            const userDoc = await getDoc(userDocRef);
+            return userDoc.data().username;
+          });
+
+          const names = await Promise.all(namesPromises);
+
+          if (names.length === 1) {
+            setOneLike(true);
+            setOnelikeName(names[0]);
+          } else {
+            setOneLike(false);
+            setOnelikeName(names[0]);
+            setAllNames(names.slice(1));
+            setMorethan(true);
+          }
+        }
+      },
+      error => {
+        console.error('Error al obtener los likes:', error);
+      },
+    );
+
+    return () => unsubscribe();
+  }, [idpub]);
+
+  const saveComment = async () => {
+    try {
+      const commentsQuery = query(
+        collection(db, 'users'),
+        where('mail', '==', user.email),
+      );
+      const commentsSnapshot = await getDocs(commentsQuery);
+      const commentsData = commentsSnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      if (commentsData.length > 0) {
+        const {id, img_profile, username} = commentsData[0];
+
+        await addDoc(collection(db, 'comments'), {
+          data: comment,
+          date: serverTimestamp(),
+          id_pub: idpub,
+          id_user: id,
+          img_perfil: img_profile,
+          username: username,
+        });
+
+        // Opcional: limpiar el campo de comentario después de guardarlo
+        setComment('');
+      } else {
+        console.error('No user data found.');
+      }
+    } catch (error) {
+      console.error('Error al obtener los comentarios:', error);
+    }
+  };
+
+  const getLikesCount = () => {};
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+            <MaterialC name="close" size={25} color={'white'} />
+          </TouchableOpacity>
+          {/* This is header of the comments section */}
+          <View style={styles.headerlikes}>
+            {isOneLike ? (
+              <>
+                <MaterialC name="cards-heart" size={25} color={'red'} />
+                <Text style={styles.headerText}>Le gusta a {oneLike}</Text>
+              </>
+            ) : morethanone ? (
+              <>
+                <MaterialC name="cards-heart" size={25} color={'red'} />
+                <Text style={styles.headerText}>
+                  Le gusta a {oneLike} y {allNames.length} más
+                </Text>
+              </>
+            ) : (
+              <Text style={styles.headerText}>No hay reacciones</Text>
+            )}
+          </View>
+          <ScrollView style={{width: '100%'}}>
+            {/* Comment by user */}
+            {datacomments.map((item, index) => (
+              <CommentUser
+                key={index}
+                data={item.data}
+                user={item.username}
+                img={item.img_perfil}
+                date={item.date}
+              />
+            ))}
+          </ScrollView>
+          <View style={styles.inputs}>
+            <TextInput
+              style={styles.input}
+              placeholder="Escribe un comentario..."
+              placeholderTextColor={'gray'}
+              value={comment}
+              onChangeText={text => setComment(text)}
+            />
+            <TouchableOpacity onPress={() => saveComment()}>
+              <MaterialC
+                style={{padding: 10, marginTop: 10}}
+                name="send"
+                size={35}
+                color={'white'}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
 
 const styles = StyleSheet.create({
-    centeredView: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        marginBottom: 20,
+  centeredView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  modalView: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#353535',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    shadowColor: '#000',
+    paddingTop: 10,
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    modalView: {
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#353535',
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        shadowColor: '#000',
-        paddingTop:10,
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-    },
-    closeButton: {
-        alignSelf: 'flex-end',
-    },
-    headerText: {
-        fontSize: 14,
-        fontWeight: 'bold',
-        color: 'white',
-        marginLeft: 5
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: '#d9d9d9',
-        borderRadius: 10,
-        padding: 10,
-        marginTop: 10,
-        width: '90%'
-    },
-    headerlikes: {
-        flexDirection: 'row',
-        marginTop: -25,
-        marginBottom: 20,
-    },
-    inputs: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    }
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  closeButton: {
+    alignSelf: 'flex-end',
+  },
+  headerText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'white',
+    marginLeft: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#d9d9d9',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+    width: '90%',
+  },
+  headerlikes: {
+    flexDirection: 'row',
+    marginTop: -25,
+    marginBottom: 20,
+  },
+  inputs: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
 
 export default CommentsComp;
